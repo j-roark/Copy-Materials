83205f10b7ec6348312afe0add87a64bfbaed0cb
j-roark <65353891+j-roark@users.noreply.github.com>
2020-09-20 01:12:57 -0400

    Update main.py

--- main.py
+++ main.py
@@ -4,12 +4,11 @@ import time
 
 mats  = ['eyeball_r', 'eyeball_l', 'eyeball_r_blue', 'eyeball_l_blue', 'eyeball_r_green', 'eyeball_l_green', 'mouth', 'hat', 'tc_beanies', 'tc_gasmask', 'tc_gasmask_glass', 'tc_helmet', 'hats', 'tc_male_citizen_torso_refugee', 'tc_male_citizen_torso1', 'tc_male_citizen_torso2', 'tc_male_citizen_torso_medic', 'tc_loyalist_suit', 'tc_pin', 'tc_male_loyalist_torso1', 'tc_labcoat', 'tc_male_citizen_worker1', 'tc_worker_armband', 'tc_male_citizen_worker2', 'tc_male_citizen_rebel', 'tc_male_citizen_rebel_why', 'tc_male_citizen_torso2', 'tc_citizen_trenchcoat', 'coat1', 'coat2a4a5a6', 'coat3', 'coat7', 'coat8', 'coat9', 'coat10', 'tc_male_citizen_torso_refugee', 'tc_male_citizen_pants_blue', 'legs_female', 'tc_male_citizen_rebel', 'shoes', 'booties', 'bootsandformalshoes', 'bootsandformalshoes', 'hands_b', 'hands_w', 'hands_w_glove', 'hands_glove', 'hands_b_glove', 'bags', 'bags', 'glass2', 'glassesfront_walter', 'glassesside_walter', 'tc_facewrap', 'facialhair02', 'tc_female_torso_refugee', 'tc_female_citizen_torso1', 'tc_female_citizen_torso2', 'tc_female_citizen_torso_medic', 'tc_loyalist_femsuit', 'tc_female_loyalist_torso1', 'tc_female_citizen_worker1', 'tc_female_citizen_worker2', 'tc_female_citizen_rebel', 'tc_female_citizen_rebel_why', 'coat1', 'coat2a4a5a6', 'coat3', 'coat7', 'coat8', 'coat9', 'coat10', 'shoes', 'female_hands', 'female_hands_black', 'female_hands_glove', 'female_hands_glove2', 'female_hands_glove2b']
 
-# ^^ Material files go here
+# ^^ Materials go here
 # Inserted are some simple examples
 
 params = ['$basetexture','$bumpmap','$Iris','$AmbientOcclTexture','$Envmap','$CorneaTexture','$phongexponenttexture', '$detail']
-
-# Insert your desired parameters
+# Insert your desired parameters to look for
 
 path = 'C:/example/path/use/forward/slashes/thanks/old/'
 newdir = 'C:/example/path/use/forward/slashes/thanks/new'
